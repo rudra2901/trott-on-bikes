3379a1efd6599bcfe430b70c352fbe44635fe9cd
Rudra Tiwari <rudratiwari2901@gmail.com>
2021-12-09 10:30:15 +0530

    fix: added functionality of timer fragment

--- app/src/main/java/com/example/trottonbikes/BikeBookedActivity.java
+++ app/src/main/java/com/example/trottonbikes/BikeBookedActivity.java
@@ -2,6 +2,7 @@ package com.example.trottonbikes;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -16,5 +17,7 @@ public class BikeBookedActivity extends AppCompatActivity {
         actionBar.hide();
 
         setContentView(R.layout.activity_bike_booked);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.bookTimeFL, new TimeRemainingFragment()).commit();
     }
 }
--- app/src/main/java/com/example/trottonbikes/TimeRemainingFragment.java
+++ app/src/main/java/com/example/trottonbikes/TimeRemainingFragment.java
@@ -3,10 +3,18 @@ package com.example.trottonbikes;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +34,36 @@ public class TimeRemainingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_remaining, container, false);
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_bike_option, container, false);
+
+        TextView timeText = rootView.findViewById(R.id.timeLayout);
+
+        // Time is in millisecond so 50sec = 50000 I have used
+        // countdown Interval is 1sec = 1000 I have used
+        new CountDownTimer(50000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // Used for formatting digit to be in 2 digits only
+                NumberFormat f = new DecimalFormat("00");
+                long hour = (millisUntilFinished / 3600000) % 24;
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+                timeText.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
+            }
+            // When the task is over it will print 00:00:00 there
+            public void onFinish() {
+                timeText.setText("00:00:00");
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.bookTimeFL, new TimeOverFragment()).commit();
+            }
+        }.start();
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //TODO: Destroy timer to stop memory leak
     }
 }
