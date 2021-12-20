b07f107d9b55a98a8b3bb2b0fdf32183968c0841
Rudra Tiwari <rudratiwari2901@gmail.com>
2021-12-20 10:41:37 +0530

    feat: changed gradle version, needed android studio update. Added google maps sdk support

--- app/src/main/java/com/example/trottonbikes/BikeActivity.java
+++ app/src/main/java/com/example/trottonbikes/BikeActivity.java
@@ -1,19 +1,52 @@
 package com.example.trottonbikes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.trottonbikes.databinding.ActivityBikeBinding;
+import com.example.trottonbikes.databinding.ActivityBikeListBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 public class BikeActivity extends AppCompatActivity {
+
+    ActivityBikeBinding binding;
+    DatabaseReference databaseReference;
+    String bikeID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bike);
+        binding = ActivityBikeBinding.inflate(getLayoutInflater());
+        View view =  binding.getRoot();
+        setContentView(view);
 
-        //TODO: book fragment doesnt work properly, make it scrollable
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.btnFL, new BikeOptionFragment()).commit();
+
+        Bundle extras = getIntent().getExtras();
+        bikeID = extras.getString("id");
+
+        databaseReference = FirebaseDatabase.getInstance("https://learnfirebase-61b73-default-rtdb.asia-southeast1.firebasedatabase.app").getReference();
+        ValueEventListener bikeListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                //Bike bike = snapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        };
     }
 }
