441c7a92ff42b2a5f8f28fa8981d05d628df8dd4
Rudra Tiwari <51710922+rudra2901@users.noreply.github.com>
2022-01-27 13:29:00 +0530

    Navigation Drawer (#19)
    * added navigation drawer
    
    * fix: fixed navigation drawer menu

--- app/src/main/java/com/example/trottonbikes/BikeListActivity.java
+++ app/src/main/java/com/example/trottonbikes/BikeListActivity.java
@@ -2,13 +2,20 @@ package com.example.trottonbikes;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.example.trottonbikes.databinding.ActivityBikeListBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,11 +26,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class BikeListActivity extends AppCompatActivity {
+public class BikeListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ArrayList<Bike> bikeArrayList;
     ActivityBikeListBinding binding;
 
+    ActionBar actionBar;
+    ActionBarDrawerToggle toggle;
     private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,13 @@ public class BikeListActivity extends AppCompatActivity {
         binding = ActivityBikeListBinding.inflate(getLayoutInflater());
         View view =  binding.getRoot();
         setContentView(view);
+
+        actionBar = getSupportActionBar();
+        toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        binding.navView.setNavigationItemSelectedListener(this);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         databaseReference = FirebaseDatabase.getInstance("https://learnfirebase-61b73-default-rtdb.asia-southeast1.firebasedatabase.app").
                 getReference("bikes");
@@ -77,5 +93,33 @@ public class BikeListActivity extends AppCompatActivity {
         binding.bikeListView.setAdapter(adapter);
 
         binding.loadingSpinner.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        if(item.getItemId() == R.id.nav_item_log_out) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(BikeListActivity.this,SignInActivity.class));
+        } else if(item.getItemId() == R.id.nav_item_profile) {
+            //TODO: make a profile page
+            Toast.makeText(BikeListActivity.this, "Opening Profile Page", Toast.LENGTH_SHORT).show();
+            //startActivity(new Intent(AvailableSlotActivity.this,ChangePinPopupActivity.class));
+        } else if(item.getItemId() == R.id.nav_item_settings) {
+            //TODO: make a settings page
+            Toast.makeText(BikeListActivity.this, "Opening Settings Page", Toast.LENGTH_SHORT).show();
+            //startActivity(new Intent(AvailableSlotActivity.this,ChangePinPopupActivity.class));
+        } else if(item.getItemId() == R.id.nav_item_register_bike) {
+            startActivity(new Intent(BikeListActivity.this,RegisterBikeActivity.class));
+        }
+        return false;
     }
 }
